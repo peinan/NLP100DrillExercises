6f08c2ccc964ab69a4f7838c9265a32a5062a466
Peinan ZHANG <peinan7@gmail.com>
2015-01-25 16:15:48 +0900

    added q078 to q079

---
+++ q078_classias-test.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+cat work/english_1.f| classias-tag -s t -m english.model -r > q078_classias-test.result
---
+++ q079_classias-test-val.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+classias-train -st -tm -g5 -x work/english_*.f > q079_classias-test-val.result
